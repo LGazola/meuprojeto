cc2bfb6e70d2471cefd6ab3a19d8dbc675468afc
LGazola <luizgazolafortuna@gmail.com>
2024-06-05 21:16:12 -0300

    carrosel de imagens com javascript

--- paginas/home.php
+++ paginas/home.php
@@ -1,1 +1,23 @@
-<h1>home</h1>
+
+<main>
+<div class="carrosel">
+<div class="container" id="img">
+    <img src="imagens/acabouchorare.jpeg" alt="Álbum">
+    <img src="imagens/cartola1976.jpeg" alt="Álbum">
+    <img src="imagens/clubedaesquina.jpeg" alt="Álbum">
+    <img src="imagens/construcao.jpeg" alt="Álbum">
+    <img src="imagens/courtofkingcrimson.jpeg" alt="Álbum">
+    <img src="imagens/deathconsciousness.jpeg" alt="Álbum">
+    <img src="imagens/larks.jpeg" alt="Álbum">
+    <img src="imagens/lysflath.jpeg" alt="Álbum">
+    <img src="imagens/madvillany.jpeg" alt="Álbum">
+    <img src="imagens/MrMorale.jpeg" alt="Álbum">
+    <img src="imagens/ncudaod.jpeg" alt="Álbum">
+    <img src="imagens/okcomputer.jpeg" alt="Álbum">
+    <img src="imagens/thebends.jpeg" alt="Álbum">
+    <img src="imagens/tomjobim.jpeg" alt="Álbum">
+    <img src="imagens/ye.jpeg" alt="Álbum">
+
+</div>
+</div>
+</main>
