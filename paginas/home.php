e217ab53c6eaac7cd4f0da0b10639dace15f52fe
LGazola <luizgazolafortuna@gmail.com>
2024-06-06 21:41:54 -0300

    carrossel e carrd v2

--- paginas/home.php
+++ paginas/home.php
@@ -38,15 +38,15 @@
     <div class="card">
     <div class="card text-center">
   <div class="card-header">
-    Featured
+    Jogos
   </div>
   <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <h5 class="card-title">Descrição dos jogos</h5>
+    <p class="card-text">saiba mais dos jogos antes de jogar, suas histórias, controles, mecânicas, etc.</p>
+    <a href="#" class="btn btn-primary">Jogos</a>
   </div>
   <div class="card-footer text-body-secondary">
-    2 days ago
+    
   </div>
 </div>
     </div>
