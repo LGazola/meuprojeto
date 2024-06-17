2d9452c258ee8c65cd6011de61a15a42f155b112
LGazola <luizgazolafortuna@gmail.com>
2024-06-17 15:45:03 -0300

    responsividade do site e aba de equipe

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ const imgs = document.getElementById("img");
 const img = document.querySelectorAll("#img img");
 
 let idx = 0;
+let offset = getOffset();
+
+function getOffset() {
+    if (window.matchMedia("(max-width: 601px)").matches) {
+      return 600; 
+    } else if (window.matchMedia("(max-width: 900px)").matches) {
+      return 900; 
+    } else {
+      return 1514; 
+    }
+  }
 
 function carrosel(){
     idx++
@@ -9,7 +20,7 @@ function carrosel(){
     if(idx > img.length - 1){
         idx=0
     }
-    imgs.style.transform = `translateX(${-idx * 550}px)`;
+    imgs.style.transform = `translateX(${-idx * offset}px)`;
     
 }
 interval = setInterval(carrosel, 1800);
